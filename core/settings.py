d035d1563604ea6ef84481a62b662f4fe0a6a085
aaqidmasoodi <aaqidmasoodi@icloud.com>
2022-08-09 02:20:59 +0530

    final commit to heroku

--- core/settings.py
+++ core/settings.py
@@ -8,6 +8,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+API_CUKBRS_VERSION = 1.0
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
