7f9d50f459af63a22a386fbe87e25d9412383e63
aaqidmasoodi <aaqidmasoodi@icloud.com>
2022-09-03 00:13:08 +0530

    ready for prod

--- accounts/utils.py
+++ accounts/utils.py
@@ -13,9 +13,9 @@ def send_otp(phone: str) -> int or None:
         otp = random.randint(1000, 9999)
         print(f"{otp} is your OTP for {phone}.")
         try:
-            # requests.get(
-            #     f"https://2factor.in/API/V1/{OTP_2FACTOR_API_KEY}/SMS/{phone}/{otp}/cusbrs_template"
-            # )
+            requests.get(
+                f"https://2factor.in/API/V1/{OTP_2FACTOR_API_KEY}/SMS/{phone}/{otp}/cusbrs_template"
+            )
             return otp
         except:
             return None
