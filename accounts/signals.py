dbc510397a54897035839d06d6381c5742ededb0
aaqidmasoodi <aaqidmasoodi@icloud.com>
2022-09-03 00:30:03 +0530

    fixed issue with django signal not saving profile

--- accounts/signals.py
+++ accounts/signals.py
@@ -14,5 +14,5 @@ def create_profile(sender, instance, created, **kwargs):
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, **kwargs):
+def save_profile(sender, instance, **kwargs):
     instance.profile.save()
