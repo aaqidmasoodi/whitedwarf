3e4d6fd2a5e8cd53b7d07d906373b995727848fe
aaqidmasoodi <aaqidmasoodi@icloud.com>
2022-09-08 20:31:01 +0530

    testing get user model

--- buses/consumers.py
+++ buses/consumers.py
@@ -1,6 +1,10 @@
 from channels.consumer import AsyncConsumer
 from channels.exceptions import StopConsumer
 from channels.db import database_sync_to_async
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class LiveLocationConsumer(AsyncConsumer):
