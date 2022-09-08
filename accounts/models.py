b1114b157cb8f0d4734029047719207a2dae49eb
aaqidmasoodi <aaqidmasoodi@icloud.com>
2022-09-08 20:48:29 +0530

    removed auto import

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,3 @@
-from email.policy import default
-from statistics import mode
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from buses.models import Bus
